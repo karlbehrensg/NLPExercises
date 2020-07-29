0dc8bd3332ef548028dbcd6026f4b1e5037156d3
Karl Behrens <karlbehrensg@gmail.com>
2020-07-29 12:25:33 -0400

    Exercises

---
+++ Exercise2.py
@@ -0,0 +1,142 @@
+import os
+import io
+import csv
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+vocab_size = 1000
+embedding_dim = 16
+max_length = 120
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = '<OOV>'
+training_portion = 0.8
+
+sentences = []
+labels = []
+stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at",
+             "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do",
+             "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having",
+             "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how",
+             "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself",
+             "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought",
+             "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should",
+             "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then",
+             "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
+             "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were",
+             "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why",
+             "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself",
+             "yourselves"]
+print(len(stopwords))
+
+with open(os.path.join('datasets', 'bbc-text.csv'), 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    for row in reader:
+        labels.append(row[0])
+        sentence = row[1]
+        for word in stopwords:
+            token = ' ' + word + ' '
+            sentence = sentence.replace(token, ' ')
+            sentence = sentence.replace('  ', ' ')
+        sentences.append(sentence)
+
+print(len(labels))
+print(len(sentences))
+print(sentences[0])
+
+train_size = int(len(labels) * training_portion)
+
+train_sentences = sentences[:train_size]
+train_labels = labels[:train_size]
+
+validation_sentences = sentences[train_size:]
+validation_labels = labels[train_size:]
+
+print(train_size)
+print(len(train_sentences))
+print(len(train_labels))
+print(len(validation_sentences))
+print(len(validation_labels))
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(train_sentences)
+word_index = tokenizer.word_index
+
+train_sequences = tokenizer.texts_to_sequences(train_sentences)
+train_padded = pad_sequences(train_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+print(len(train_sequences[0]))
+print(len(train_padded[0]))
+
+print(len(train_sequences[1]))
+print(len(train_padded[1]))
+
+print(len(train_sequences[10]))
+print(len(train_padded[10]))
+
+validation_sequences = tokenizer.texts_to_sequences(validation_sentences)
+validation_padded = pad_sequences(validation_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+print(len(validation_sequences))
+print(validation_padded.shape)
+
+label_tokenizer = Tokenizer()
+label_tokenizer.fit_on_texts(labels)
+
+training_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+validation_label_seq = np.array(label_tokenizer.texts_to_sequences(validation_labels))
+
+print(training_label_seq[0])
+print(training_label_seq[1])
+print(training_label_seq[2])
+print(training_label_seq.shape)
+
+print(validation_label_seq[0])
+print(validation_label_seq[1])
+print(validation_label_seq[2])
+print(validation_label_seq.shape)
+
+model = tf.keras.Sequential([
+    keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    keras.layers.GlobalAveragePooling1D(),
+    keras.layers.Dense(24, activation='relu'),
+    keras.layers.Dense(6, activation='sigmoid')
+])
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+num_epochs = 30
+history = model.fit(train_padded, training_label_seq, epochs=num_epochs,
+                    validation_data=(validation_padded, validation_label_seq), verbose=2)
+
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_' + string])
+    plt.xlabel('Epochs')
+    plt.ylabel(string)
+    plt.legend([string, 'val_' + string])
+    plt.show()
+
+
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+
+def decode_sentence(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape)
